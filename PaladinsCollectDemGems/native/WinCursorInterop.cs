112dabbbaf5ed5b04ffdba104aa58533d8dc2123
Brandon Porter <brandon.porter18@gmail.com>
2017-04-25 09:41:49 -0700

    Created a virtual mouse with a wrapper around windows native cursor events

---
+++ PaladinsCollectDemGems/native/WinCursorInterop.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PaladinsCollectDemGems.native
+{
+	/// <summary>
+	/// Provides access to the windows native layer
+	/// </summary>
+	public static class WinCursorInterop
+	{
+		/// <summary>
+		/// Mouse actions you can perform
+		/// </summary>
+		[Flags]
+		public enum MouseEvent
+		{
+			Move = 0x0001,
+			LeftClickDown = 0x0002,
+			LeftClickUp = 0x0004,
+			RightClickDown = 0x0008,
+			RightClickUp = 0x0010,
+			MiddleClickDown = 0x0020,
+			MiddleClickUp = 0x0040,
+			Absolute = 0x8000
+		};
+
+		//This is a replacement for Cursor.Position in WinForms
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern bool SetCursorPos(int x, int y);
+
+		[System.Runtime.InteropServices.DllImport("user32.dll")]
+		private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+		/// <summary>
+		/// Moves the cursor to the specified screen coordinates. If the new coordinates are not within the screen rectangle set by the most
+		/// recent ClipCursor function call, the system automatically adjusts the coordinates so that the cursor stays within the rectangle.
+		/// </summary>
+		/// <param name="x">new x-coordinate of the cursor, in screen coordinates</param>
+		/// <param name="y">new y-coordinate of the cursor, in screen coordinates</param>
+		/// <returns>true for success, false for failure</returns>
+		public static bool SetCursorPosition(int x, int y)
+		{
+			return SetCursorPos(x, y);
+		}
+
+		/// <summary>
+		/// Synthesizes mouse motion and button clicks. See <see href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx">Docs</see> 
+		/// for more info 
+		/// </summary>
+		/// <param name="dwFlags">Controls various aspects of mouse motion and button clicking </param>
+		/// <param name="dx">mouse's absolute position along the x-axis</param>
+		/// <param name="dy">mouse's absolute position along the y-axis</param>
+		/// <param name="dwData">specific data related to the type of event</param>
+		/// <param name="dwExtraInfo">an additional value associated with the mouse event</param>
+		public static void SendMouseEvent(int dwFlags, int dx, int dy, int dwData = 0, int dwExtraInfo = 0)
+		{
+			mouse_event(dwFlags, dx, dy, dwData, dwExtraInfo);
+		}
+
+		/// <summary>
+		/// Synthesizes mouse motion and button clicks. See <see href="https://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx">Docs</see> 
+		/// for more info 
+		/// </summary>
+		/// <param name="mouseEvent">friendly bitwise value to pass as dwFlags to represent the mouse event</param>
+		/// <param name="dx">mouse's absolute position along the x-axis</param>
+		/// <param name="dy">mouse's absolute position along the y-axis</param>
+		/// <param name="dwData">specific data related to the type of event</param>
+		/// <param name="dwExtraInfo">an additional value associated with the mouse event</param>
+		public static void SendMouseEvent(MouseEvent mouseEvent, int dx, int dy, int dwData = 0, int dwExtraInfo = 0)
+		{
+			SendMouseEvent((int)mouseEvent, dx, dy, dwData, dwExtraInfo);
+		}
+	}
+}
---
+++ PaladinsCollectDemGems/tools/Mouse.cs
@@ -0,0 +1,99 @@
+﻿using PaladinsCollectDemGems.native;
+using System.Drawing;
+using System.Windows.Forms;
+using MouseEvent = PaladinsCollectDemGems.native.WinCursorInterop.MouseEvent;
+
+namespace PaladinsCollectDemGems.tools
+{
+	/// <summary>
+	/// A virtual mouse which delegates all actions to the desktop mouse
+	/// </summary>
+	public static class Mouse
+	{
+		/// <summary>
+		/// Performs a left click at the current cursor position
+		/// </summary>
+		public static void LeftClick()
+		{
+			Point currPos = GetMousePosition();
+			LeftClick(currPos.X, currPos.Y);
+		}
+
+		/// <summary>
+		/// Performs a left click at the specified cursor position
+		/// </summary>
+		/// <param name="xPos">x position in screen coordinates</param>
+		/// <param name="yPos">y position in screen coordinates</param>
+		/// <param name="retainClickPosition">determines if the cursor should stay at the click position, or reset to the starting before the click</param>
+		public static void LeftClick(int xPos, int yPos, bool retainClickPosition = true)
+		{
+			ExecuteMouseEvents(xPos, yPos, retainClickPosition, MouseEvent.LeftClickDown, MouseEvent.LeftClickUp);
+		}
+
+		/// <summary>
+		/// Performs a right click at the current cursor position
+		/// </summary>
+		public static void RightClick()
+		{
+			Point currPos = GetMousePosition();
+			RightClick(currPos.X, currPos.Y);
+		}
+
+		/// <summary>
+		/// Performs a right click at the specified cursor position
+		/// </summary>
+		/// <param name="xPos">x position in screen coordinates</param>
+		/// <param name="yPos">y position in screen coordinates</param>
+		/// <param name="retainClickPosition">determines if the cursor should stay at the click position, or reset to the starting before the click</param>
+		public static void RightClick(int xPos, int yPos, bool retainClickPosition = true)
+		{
+			ExecuteMouseEvents(xPos, yPos, retainClickPosition, MouseEvent.RightClickDown, MouseEvent.RightClickUp);
+		}
+
+		/// <summary>
+		/// Moves the mouse position to the specified xy position in screen coordinates
+		/// </summary>
+		/// <param name="xPos">x position in screen coordinates</param>
+		/// <param name="yPos">y position in screen coordinates</param>
+		public static void MoveTo(int xPos, int yPos)
+		{
+			WinCursorInterop.SetCursorPosition(xPos, yPos);
+		}
+
+		/// <summary>
+		/// Retrieves the current mouse position
+		/// </summary>
+		/// <returns>Point struct containing current x-y cursor position in screen coordinates</returns>
+		public static Point GetMousePosition()
+		{
+			return Control.MousePosition;
+		}
+
+
+		/// <summary>
+		/// Executes a set of mouse events at a specific position on the screen
+		/// </summary>
+		/// <param name="xPos">x position in screen coordinates</param>
+		/// <param name="yPos">y position in screen coordinates</param>
+		/// <param name="retainNewPosition">determines if the cursor should stay at the event position, or reset to the starting before the events</param>
+		/// <param name="mouseEvents">the mouse events to execute</param>
+		private static void ExecuteMouseEvents(int xPos, int yPos, bool retainNewPosition, params MouseEvent[] mouseEvents)
+		{
+			Point currPos = GetMousePosition();
+
+			// First move the cursor (must be at the position before executing the events)
+			MoveTo(xPos, yPos);
+
+			// Send all of the mouse events
+			foreach (MouseEvent mEvent in mouseEvents) {
+				WinCursorInterop.SendMouseEvent(mEvent, xPos, yPos);
+			}
+
+			if (!retainNewPosition)
+			{
+				// Reset to original mouse position before the events
+				MoveTo(currPos.X, currPos.Y);
+			}
+		}
+	}
+}

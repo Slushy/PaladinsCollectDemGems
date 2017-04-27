38e604755509b5906f05d49e39e88ae7c6486d4d
Brandon Porter <brandon.porter18@gmail.com>
2017-04-26 21:46:44 -0700

    Created some custom exceptions for a game's invalid state possibility

---
+++ PaladinsCollectDemGems/exceptions/GameNotInstalledException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PaladinsCollectDemGems.exceptions
+{
+	/// <summary>
+	/// An exception for if a requested game is not installed
+	/// </summary>
+	public class GameNotInstalledException : Exception
+	{
+		/// <summary>
+		/// Initializes new instance of GameNotInstalledException class
+		/// </summary>
+		public GameNotInstalledException()
+		{
+		}
+
+		/// <summary>
+		/// Initializes new instance of GameNotInstalledException class with a specified error message
+		/// </summary>
+		/// <param name="message">the message that explains the reason for the error</param>
+		public GameNotInstalledException(string message)
+		: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes new instance of GameNotInstalledException class with a specified error message and 
+		/// a reference to the inner exception that is the cause of this exception
+		/// </summary>
+		/// <param name="message">the message that explains the reason for the error</param>
+		/// <param name="innerException">the inner exception that caused this exception</param>
+		public GameNotInstalledException(string message, Exception innerException)
+		: base(message, innerException)
+		{
+		}
+	}
+}
---
+++ PaladinsCollectDemGems/exceptions/GameUpdatingException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PaladinsCollectDemGems.exceptions
+{
+	/// <summary>
+	/// An exception for if the game's state is updating and unavailable for control
+	/// </summary>
+	public class GameUpdatingException : Exception
+	{
+		/// <summary>
+		/// Initializes new instance of GameUpdatingException class
+		/// </summary>
+		public GameUpdatingException()
+		{
+		}
+
+		/// <summary>
+		/// Initializes new instance of GameUpdatingException class with a specified error message
+		/// </summary>
+		/// <param name="message">the message that explains the reason for the error</param>
+		public GameUpdatingException(string message)
+		: base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes new instance of GameUpdatingException class with a specified error message and 
+		/// a reference to the inner exception that is the cause of this exception
+		/// </summary>
+		/// <param name="message">the message that explains the reason for the error</param>
+		/// <param name="innerException">the inner exception that caused this exception</param>
+		public GameUpdatingException(string message, Exception inner)
+		: base(message, inner)
+		{
+		}
+	}
+}

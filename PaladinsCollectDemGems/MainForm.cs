722ffe5ae5a638cb79a83e57eb5a38971ef98385
Brandon Porter <brandon.porter18@gmail.com>
2017-04-26 22:04:41 -0700

    Created steam class to access active user from registry

--- PaladinsCollectDemGems/MainForm.cs
+++ PaladinsCollectDemGems/MainForm.cs
@@ -1,12 +1,30 @@
-﻿using System.Windows.Forms;
+﻿using PaladinsCollectDemGems.game;
+using System;
+using System.Windows.Forms;
 
 namespace PaladinsCollectDemGems
 {
 	public partial class MainForm : Form
 	{
+		// Move to separate config file or something
+		private const int PALADINS_STEAM_ID = 444090;
+
 		public MainForm()
 		{
 			InitializeComponent();
+
+			try {
+				// why & such?
+				if (Steam.IsLoggedIn)
+					Console.WriteLine("User is logged in: {0}", Steam.ActiveUserId);
+				else
+					Console.WriteLine("No valid steam user");
+			}
+			catch (Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+
+			Console.WriteLine("We did it reddit");
 		}
 	}
 }
---
+++ PaladinsCollectDemGems/game/Steam.cs
@@ -0,0 +1,28 @@
+﻿using PaladinsCollectDemGems.tools.native;
+
+namespace PaladinsCollectDemGems.game
+{
+	/// <summary>
+	/// Class that acts as the base for launching Steam games
+	/// </summary>
+	public static class Steam
+	{
+		public const string BASE_REGISTRY_LOCATION = "Software\\Valve\\Steam\\";
+		public const string PROCESS_REGISTRY_LOCATION = BASE_REGISTRY_LOCATION + "ActiveProcess";
+		
+		/// <summary>
+		/// Gets the currently logged in steam userId, or null if n/a
+		/// </summary>
+		public static int? ActiveUserId {
+			get { 
+				int? userId = WinRegistry.GetValueAsInt(PROCESS_REGISTRY_LOCATION, "ActiveUser");
+				return userId.HasValue && userId > 0 ? userId : null;	
+			}
+		}
+
+		/// <summary>
+		/// Checks if we have a valid steam user logged in
+		/// </summary>
+		public static bool IsLoggedIn { get { return ActiveUserId.HasValue; } }
+	}
+}

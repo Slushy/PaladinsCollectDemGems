bd0d1d59a22c7f511a23771be144b2e2cfd62087
Brandon Porter <brandon.porter18@gmail.com>
2017-04-26 22:44:01 -0700

    Added ability to launch a steam game from the steam class

--- PaladinsCollectDemGems/MainForm.cs
+++ PaladinsCollectDemGems/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using PaladinsCollectDemGems.game;
+﻿using PaladinsCollectDemGems.exceptions;
+using PaladinsCollectDemGems.game;
 using System;
 using System.Windows.Forms;
 
@@ -7,23 +8,33 @@ namespace PaladinsCollectDemGems
 	public partial class MainForm : Form
 	{
 		// Move to separate config file or something
-		private const int PALADINS_STEAM_ID = 444090;
+		private const int PALADINS_STEAM_GAME_ID = 444090;
 
 		public MainForm()
 		{
 			InitializeComponent();
 
 			try {
-				// why & such?
-				if (Steam.IsLoggedIn)
-					Console.WriteLine("User is logged in: {0}", Steam.ActiveUserId);
-				else
-					Console.WriteLine("No valid steam user");
+				SteamGame paladins = Steam.LaunchGame(PALADINS_STEAM_GAME_ID);
+				
+				MessageBox.Show("Success");
+			}
+			catch (InvalidSteamUserException ex) {
+				MessageBox.Show("Invalid Steam User");
+			}
+			catch (GameNotInstalledException ex)
+			{
+				MessageBox.Show("Game is invalid or not installed");
+			}
+			catch (GameUpdatingException ex)
+			{
+				MessageBox.Show("Game is currently updating and in bad state");
 			}
 			catch (Exception ex) {
-				Console.WriteLine(ex.Message);
+				MessageBox.Show("Unknown error occurred: {0}", ex.Message);
 			}
 
+			
 			Console.WriteLine("We did it reddit");
 		}
 	}
--- PaladinsCollectDemGems/game/Steam.cs
+++ PaladinsCollectDemGems/game/Steam.cs
@@ -1,4 +1,7 @@
-﻿using PaladinsCollectDemGems.tools.native;
+﻿using PaladinsCollectDemGems.exceptions;
+using PaladinsCollectDemGems.tools.native;
+using System.Diagnostics;
+using System.Threading;
 
 namespace PaladinsCollectDemGems.game
 {
@@ -7,9 +10,12 @@ namespace PaladinsCollectDemGems.game
 	/// </summary>
 	public static class Steam
 	{
-		public const string BASE_REGISTRY_LOCATION = "Software\\Valve\\Steam\\";
-		public const string PROCESS_REGISTRY_LOCATION = BASE_REGISTRY_LOCATION + "ActiveProcess";
-		
+		// Move these to config file too, or who really cares?
+		public  const string BASE_REGISTRY_LOCATION = "Software\\Valve\\Steam\\";
+
+		private const string PROCESS_REGISTRY_LOCATION = BASE_REGISTRY_LOCATION + "ActiveProcess";
+		private const string STEAM_LAUNCH_URI = "steam://rungameid/{0}";
+
 		/// <summary>
 		/// Gets the currently logged in steam userId, or null if n/a
 		/// </summary>
@@ -24,5 +30,47 @@ namespace PaladinsCollectDemGems.game
 		/// Checks if we have a valid steam user logged in
 		/// </summary>
 		public static bool IsLoggedIn { get { return ActiveUserId.HasValue; } }
+
+		/// <summary>
+		/// Launches a steam game if it is not already in the middle of launching or running. Creates a new SteamGame object which wraps
+		/// the game process and window and returns it when the game is running. Should not be run on main thread.
+		/// </summary>
+		/// <param name="gameId">Steam app ID to launch</param>
+		/// <returns>Game wrapper representing the new running steam game process and window</returns>
+		public static SteamGame LaunchGame(int gameId)
+		{
+			if (!IsLoggedIn)
+				throw new InvalidSteamUserException("User is not logged in");
+
+			SteamGame game = new SteamGame(gameId);
+
+			// Throw an exception if the game is not installed
+			if (!game.IsInstalled)
+				throw new GameNotInstalledException();
+
+			// Throw an exception if the game is updating and not usable
+			if (game.IsUpdating)
+				throw new GameUpdatingException();
+
+			// Launches the steam launcher process only if the game is not already launching
+			if (!game.IsRunning && !game.IsLaunching)
+			{
+				string steamLauncherProcessUri = string.Format(STEAM_LAUNCH_URI, gameId);
+				using (Process steamLauncherProcess = Process.Start(steamLauncherProcessUri))
+				{
+					// should almost be instant as the process' job is to
+					// create the actual game launcher process. Process-ception.
+					steamLauncherProcess.WaitForExit();
+				}
+			}
+
+			// Wait until the game's process has updated to be running
+			while (!game.IsRunning)
+				Thread.Sleep(100);
+
+			// At this point the game is running (possibly not yet foregrounded)
+			return game;
+		}
 	}
 }
+

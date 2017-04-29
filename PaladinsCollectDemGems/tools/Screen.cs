74a30afa0b20828093e1f020402e80ce6add8d61
Brandon Porter <brandon.porter18@gmail.com>
2017-04-28 23:27:59 -0700

    Added simple static Screen class to pull your monitor properties

---
+++ PaladinsCollectDemGems/tools/Screen.cs
@@ -0,0 +1,18 @@
+﻿namespace PaladinsCollectDemGems.tools
+{
+	/// <summary>
+	/// Accessible properties of your computer screen
+	/// </summary>
+	public static class Screen
+	{
+		/// <summary>
+		/// Width of your primary computer screen
+		/// </summary>
+		public static int Width { get { return System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width; } }
+
+		/// <summary>
+		/// Height of your primary computer screen
+		/// </summary>
+		public static int Height { get { return System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height; } }
+	}
+}

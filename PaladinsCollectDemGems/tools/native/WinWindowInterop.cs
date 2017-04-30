4f447894a40ce6517db507631b8c4460663a47b5
Brandon Porter <brandon.porter18@gmail.com>
2017-04-30 12:50:40 -0700

    Remove unused testing methods from native window

--- PaladinsCollectDemGems/tools/native/WinWindowInterop.cs
+++ PaladinsCollectDemGems/tools/native/WinWindowInterop.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace PaladinsCollectDemGems.tools.native
@@ -60,78 +59,7 @@ namespace PaladinsCollectDemGems.tools.native
 		/// <param name="rect">the structure to be updated with the window position</param>
 		/// <returns>true if getting the window position succeeded, false otherwise</returns>
 		public static bool GetWindowPosition(IntPtr windowHandle, out RECT rect) {
-			var success = GetWindowRect(windowHandle, out rect);
-
-			//if (!success) {
-			//	var children = GetAllChildHandles(windowHandle);
-			//	Console.WriteLine("fail");
-			//}
-
-			return success;
-		}
-
-		[DllImport("user32.dll")]
-		static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-		[DllImport("user32.dll")]
-		static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-		[DllImport("user32.dll")]
-		static extern bool SetLayeredWindowAttributes(IntPtr hwnd, uint crKey, byte bAlpha, uint dwFlags);
-
-		public const int WS_EX_TOPMOST = 0x00000008;
-		public const int GWL_EXSTYLE = -20;
-		public const int WS_EX_LAYERED = 0x80000;
-		public const int LWA_ALPHA = 0x2;
-		private const int LWA_COLORKEY = 0x1;
-
-		public static bool SetColorThing(IntPtr windowHandle)
-		{
-			Console.WriteLine("WTF");
-			SetWindowLong(windowHandle, GWL_EXSTYLE, GetWindowLong(windowHandle, GWL_EXSTYLE) ^ ( WS_EX_LAYERED | WS_EX_TOPMOST ));
-			return SetLayeredWindowAttributes(windowHandle, 0, 128, LWA_ALPHA);
-		}
-
-
-		private delegate bool EnumWindowProc(IntPtr hwnd, IntPtr lParam);
-
-		[DllImport("user32")]
-		[return: MarshalAs(UnmanagedType.Bool)]
-		private static extern bool EnumChildWindows(IntPtr window, EnumWindowProc callback, IntPtr lParam);
-
-		public static List<IntPtr> GetAllChildHandles(IntPtr windowHandle)
-		{
-			List<IntPtr> childHandles = new List<IntPtr>();
-
-			GCHandle gcChildhandlesList = GCHandle.Alloc(childHandles);
-			IntPtr pointerChildHandlesList = GCHandle.ToIntPtr(gcChildhandlesList);
-
-			try
-			{
-				EnumWindowProc childProc = new EnumWindowProc(EnumWindow);
-				EnumChildWindows(windowHandle, childProc, pointerChildHandlesList);
-			}
-			finally
-			{
-				gcChildhandlesList.Free();
-			}
-
-			return childHandles;
-		}
-
-		private static bool EnumWindow(IntPtr hWnd, IntPtr lParam)
-		{
-			GCHandle gcChildhandlesList = GCHandle.FromIntPtr(lParam);
-
-			if (gcChildhandlesList == null || gcChildhandlesList.Target == null)
-			{
-				return false;
-			}
-
-			List<IntPtr> childHandles = gcChildhandlesList.Target as List<IntPtr>;
-			childHandles.Add(hWnd);
-
-			return true;
+			return GetWindowRect(windowHandle, out rect);
 		}
 	}
 }
